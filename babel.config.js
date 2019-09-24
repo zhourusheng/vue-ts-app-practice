90accf4cdedf6147931a8c2a3377c09d6de324f8
zhourusheng <nfuzrs@126.com>
2019-09-24 19:56:50 +0800

    安装vant

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
+const plugins = [
+  [
+    "import",
+    {
+      libraryName: "vant",
+      libraryDirectory: "es",
+      style: true
+    },
+    "vant"
+  ]
+];
+
 module.exports = {
+  plugins,
   presets: ["@vue/app"]
 };
