8648087488172917c7caa31d756ccb2ec34ba33a
zhourusheng <nfuzrs@126.com>
2019-09-25 11:20:45 +0800

    安装 vue-page-stack

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,75 @@
 <template>
   <div id="app">
     <transition :name="transitionName">
-      <router-view class="router-view-c" />
+      <vue-page-stack>
+        <router-view class="router-view-c" />
+      </vue-page-stack>
     </transition>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Watch, Vue } from "vue-property-decorator";
-import { Route } from "vue-router";
+import { Component, Watch, Vue } from 'vue-property-decorator'
+import { Route } from 'vue-router'
 
 @Component
 export default class Home extends Vue {
-  private transitionName = "forward";
+  private transitionName = 'forward'
 
-  @Watch("$route")
+  @Watch('$route')
   private onChildChanged(to: Route, from: Route) {
-    if (to.params["stack-key-dir"] === "forward") {
-      this.transitionName = "forward";
+    // 如果想在页面前进或者后退的时候添加一些动画，可以通过stack-key-dir进行判断
+    if (to.params['stack-key-dir'] === 'forward') {
+      this.transitionName = 'forward'
     } else {
-      this.transitionName = "back";
+      this.transitionName = 'back'
     }
   }
 
   private created() {
-    this.handleFocusOut();
-    this.handleResize();
+    this.handleFocusOut()
+    this.handleResize()
   }
 
   private handleFocusOut() {
     // input 焦点失焦后，ios 键盘收起，但没有触发 window resize，导致实际页面dom仍然被键盘顶上去--错位
-    document.addEventListener("focusout", () => {
-      document.body.scrollTop = 0;
-    });
+    document.addEventListener('focusout', () => {
+      document.body.scrollTop = 0
+    })
   }
 
   // 监听resize事件（键盘弹起触发），然后将 input textarea 元素滑动到可视区域，并将特定元素隐藏
   private handleResize() {
-    const clientHeight = document.documentElement.clientHeight;
-    window.addEventListener("resize", () => {
+    const clientHeight = document.documentElement.clientHeight
+    window.addEventListener('resize', () => {
       // 判断当前 active 的元素是否为 input 或 textarea
       if (
-        document.activeElement!.tagName === "INPUT" ||
-        document.activeElement!.tagName === "TEXTAREA"
+        document.activeElement!.tagName === 'INPUT' ||
+        document.activeElement!.tagName === 'TEXTAREA'
       ) {
         setTimeout(() => {
           // 原生方法，滚动至需要显示的位置
-          document.activeElement!.scrollIntoView();
-        }, 0);
+          document.activeElement!.scrollIntoView()
+        }, 0)
       }
 
       // 解决键盘弹起后 fixed 定位元素被顶起问题
-      const bodyHeight = document.documentElement.clientHeight;
-      const ele = document.getElementById("fixed-bottom");
+      const bodyHeight = document.documentElement.clientHeight
+      const ele = document.getElementById('fixed-bottom')
       if (ele) {
         if (clientHeight > bodyHeight) {
-          (ele as HTMLElement).style.display = "none";
+          ;(ele as HTMLElement).style.display = 'none'
         } else {
-          (ele as HTMLElement).style.display = "block";
+          ;(ele as HTMLElement).style.display = 'block'
         }
       }
-    });
+    })
   }
 }
 </script>
 
 <style lang="scss">
-@import "./assets/styles/index.scss";
+@import './assets/styles/index.scss';
 
 #app {
   position: fixed;
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import VuePageStack from 'vue-page-stack'
+import LocalConfig from '@/config.json'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
+
+if (LocalConfig.VuePageStackEnabled) {
+  Vue.use(VuePageStack, { router })
+}
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount("#app");
+  render: (h) => h(App)
+}).$mount('#app')
